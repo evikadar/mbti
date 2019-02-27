ea4987f281f4443d1011affd731ec287a1e8b280
Evi Kadar <evi.kadar@gmail.com>
2019-02-27 09:40:41 +0100

    Initial commit

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/main"})
+public class IndexController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("main.html", context, resp.getWriter());
+
+    }
+}
