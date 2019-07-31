ccf493e18631f40b0f46b4f94ce958d8d84ee767
Evi Kadar <evi.kadar@gmail.com>
2019-07-31 19:11:00 +0200

    main page with separate navigation and index.htmls

--- src/main/java/controller/RolunkController.java
+++ src/main/java/controller/RolunkController.java
@@ -12,14 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/"})
-public class IndexController extends HttpServlet {
+@WebServlet(urlPatterns = {"/rolunk"})
+public class RolunkController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        engine.process("index.html", context, resp.getWriter());
+        engine.process("rolunk.html", context, resp.getWriter());
 
     }
 
@@ -31,6 +31,6 @@ public class IndexController extends HttpServlet {
         HttpSession session = req.getSession(true);;
         session.setAttribute("person1", person1);
         session.setAttribute("person2", person2);
-        resp.sendRedirect("/main#projects");
+        resp.sendRedirect("/");
     }
 }
