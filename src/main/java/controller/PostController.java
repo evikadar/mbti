ea88401c220a444e4d2dbdd98b40b6c4d704ac58
evikadar <evi.kadar@gmail.com>
2019-08-08 12:02:43 +0200

    changed font but something is still wrong with character encoding that bug must be fixed still

--- src/main/java/controller/PostController.java
+++ src/main/java/controller/PostController.java
@@ -11,17 +11,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/"})
-public class IndexController extends HttpServlet {
+@WebServlet(urlPatterns = {"/post"})
+public class PostController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        // todo: utolsó 3 postot szedje le és küldje át az adatokat itt a contextben
-
-        engine.process("index.html", context, resp.getWriter());
+        engine.process("post.html", context, resp.getWriter());
 
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // todo: save comments here
+
+        String title = req.getParameter("title");
+        System.out.printf("Data is got is %s, %s, %s%n", title);
+
+
+        resp.sendRedirect("/blog");
+    }
+
+
 }
